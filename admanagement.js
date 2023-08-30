8382b2d3ff014bf9bd851241e6aa1f61c1fefc2e
aspirewebplix <143328085+aspirewebplix@users.noreply.github.com>
2023-08-30 16:05:32 +0800

    Update admanagement.js

--- admanagement.js
+++ admanagement.js
@@ -1,12 +1,25 @@
+// Check if ad blocker is active
+function isAdBlockerActive() {
+  var adZoneElement = document.querySelector('.ad-zone');
 
-let x = document.querySelector(".ad-zone");
-   let x_height = x.offsetHeight;
-   let msg = document.getElementById("msg")
-     
-   if(x_height){
-     msg.innerText = "No Adblocker detected"
-   } else{
-     msg.innerText = "Adblocker detected"
-     alert("Oops! Adblocker detected, please disable adblocker to use our service");
-     location.reload();
+  if (adZoneElement) {
+    var elementComputedStyle = window.getComputedStyle(adZoneElement);
+    var elementHeight = parseFloat(elementComputedStyle.getPropertyValue('height'));
+    var elementDisplay = elementComputedStyle.getPropertyValue('display');
+
+    return elementHeight === 0 || elementDisplay === 'none';
+  }
+
+  return false;
+}
+
+// Usage
+if (isAdBlockerActive()) {
+  // Ad blocker is active, take action
+  console.log("Ad blocker is active.");
+  alert("OOPS! Adblocker detected, please disable ad blocker to our service, it is stated in our terms and conditions, thank you :)");
+  location.reload();
+} else {
+  // Ad blocker is not detected
+  console.log("Ad blocker is not active.");
 }
